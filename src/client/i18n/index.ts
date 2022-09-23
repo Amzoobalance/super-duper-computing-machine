998ed86f173147c304bed0c6dd746a71127ee3d8
Sergei Orlov <orlowdev@gmail.com>
2022-09-23 13:56:59 +0300

    :sparkles: Move translations

--- src/client/i18n/index.ts
+++ src/client/i18n/index.ts
@@ -3,16 +3,19 @@ import { initReactI18next } from "react-i18next"
 
 // TODO: Add a convenient way to import these
 // TODO: Automatically append extension name `@{extension}`
-import ru from "@core/translations/ru.json"
-import en from "@core/translations/en.json"
+import ruActivityBar from "@core/activity-bar/translations/ru.json"
+import enActivityBar from "@core/activity-bar/translations/en.json"
 
-import ruSettings from "@client/app/translations/ru.json"
-import enSettings from "@client/app/translations/en.json"
+import ruNotifications from "@core/notifications/translations/ru.json"
+import enNotifications from "@core/notifications/translations/en.json"
+
+import ruApp from "@client/app/translations/ru.json"
+import enApp from "@client/app/translations/en.json"
 
 use(initReactI18next).init({
   resources: {
-    en: { translation: { ...en, ...enSettings } },
-    ru: { translation: { ...ru, ...ruSettings } },
+    en: { translation: { ...enApp, ...enActivityBar, enNotifications } },
+    ru: { translation: { ...ruApp, ...ruActivityBar, ruNotifications } },
   },
   fallbackLng: "en",
   interpolation: { escapeValue: false },
